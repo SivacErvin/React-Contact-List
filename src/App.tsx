167bc40715a62f15c39454c924071c9b33e7d041
Sivac Ervin <sivacervin96@gmail.com>
2022-03-20 15:26:15 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-octal */
+import { Button } from '@mui/material';
+import React,{ useState } from 'react';
+import './App.scss';
+import ContactsList from './Pages/ContactsList';
+import { Input } from '@mui/material';
+
+export interface Persona{
+  name: string
+  surname: string
+  number: string
+}
 
 function App() {
+
+  const persona1:Persona = {name:"Ivo", surname:"Ivić", number: "098538907"};
+  const persona2:Persona = {name:"Andro", surname:"Andrić", number: "098538909"};
+  const initialContactList:Persona[] = [persona1,persona2];
+  const [contactList, setContactsList] = useState(initialContactList);
+
+  const [person, setPerson] = useState<Persona>({name:"", surname:"", number: ""});
+  
+  function add(){
+
+    setContactsList([...contactList, person]);
+
+    setPerson({name:"", surname:"", number: ""});    
+  }
+
+
+
+  function handleInput(event: any){
+
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setPerson(values => ({...values, [name]:value}));
+
+    //console.log(person);
+  }
+
+  function deleteContact(number:string){
+
+    const newList=contactList.filter((contact)=>(number !== contact.number));
+    setContactsList(newList);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='Top'>
+        Evo promijeniti cemo ovo samo za Ivu.
+      </h1>
+
+      <div>
+        <Input name="name" value={person.name } placeholder='Name' onChange={handleInput}/>
+        <Input name="surname" value={person.surname } placeholder='Surname' onChange={handleInput}/>
+        <Input name="number" value={person.number } placeholder='Number' onChange={handleInput}/>
+        <Button variant='outlined' onClick={add} >
+          Create contact 
+        </Button>
+      </div>
+      <ContactsList contactList={contactList} deleteContact={deleteContact} />
     </div>
   );
-}
+} 
 
 export default App;
---
+++ src/Pages/ContactsList.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import { Persona } from '../App'
+
+interface ContactsListProps {
+    contactList: Persona[]
+    deleteContact: (number:string)=>any
+}
+
+function ContactsList(props:ContactsListProps) {
+
+  return (
+    <div>{props.contactList.map((pers)=> {
+        return (
+            <div>
+                {pers.name} {pers.surname} {pers.number} <Button onClick={()=>props.deleteContact(pers.number)} >Del</Button>
+            </div>
+        )
+    })}</div>
+  )
+}
+//onClick={props.deleteContact(pers.number)}
+export default ContactsList
